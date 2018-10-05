0588012d8bb68b517a30e5fc6faca2f71b6b80a4
Guilherme França <guiifrancaz@gmail.com>
2018-10-05 17:03:16 -0300

    Correção no Storyboard

--- FoundationsApp/ViewController.swift
+++ FoundationsApp/ViewController.swift
@@ -8,19 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var storyTableView: UITableView!
     
     let story1 = Story(heroImageReceived: #imageLiteral(resourceName: "example"), titleReceived: "Lorem Ipsum", descriptionReceived: "Lorem Ipsum", textReceived: "Lorem Ipsum")
     let story2 = Story(heroImageReceived: #imageLiteral(resourceName: "example"), titleReceived: "Lorem Ipsum", descriptionReceived: "Lorem Ipsum", textReceived: "Lorem Ipsum")
     let story3 = Story(heroImageReceived: #imageLiteral(resourceName: "example"), titleReceived: "Lorem Ipsum", descriptionReceived: "Lorem Ipsum", textReceived: "Lorem Ipsum")
     
+    var stories: [Story] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        stories.append(story1)
+        stories.append(story2)
+        stories.append(story3)
+        
+        storyTableView.delegate = self
+        storyTableView.dataSource = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = storyTableView.dequeueReusableCell(withIdentifier: "storyCell", for: indexPath) as! StoryTableViewCell
+        
+        let index = indexPath.row
+        
+        cell.storyImage.image = stories[index].heroImage
+        cell.storyHeadline.text = stories[index].title
+        cell.storyBrief.text = stories[index].description
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 350
+    }
 
 }
 
