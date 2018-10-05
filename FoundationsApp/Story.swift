c328eac1dc9de8713a395dc3cb31c904b870dd4c
Guilherme França <guiifrancaz@gmail.com>
2018-10-05 14:24:34 -0300

    Classe bomdia apagada

--- FoundationsApp/Story.swift
+++ FoundationsApp/Story.swift
@@ -8,20 +8,17 @@
 
 import Foundation
 import UIKit
-// Bora programar
+
 class Story {
     var heroImage: UIImage
     var title: String
     var description: String
     var text: String
-    var bomdia: Int
     
-    init (heroImageReceived: UIImage, titleReceived: String, descriptionReceived: String, textReceived: String, bomdiaReceived: Int) {
+    init (heroImageReceived: UIImage, titleReceived: String, descriptionReceived: String, textReceived: String) {
         self.heroImage = heroImageReceived
         self.title = titleReceived
         self.description = descriptionReceived
         self.text = textReceived
-        self.bomdia = bomdiaReceived
-        
     }
 }
