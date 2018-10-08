a107cdd125464ca1c9a0489e6ac2537b0eede11b
Guilherme França <guiifrancaz@gmail.com>
2018-10-08 18:05:39 -0300

    Configuração do Navigation Controller

--- FoundationsApp/ViewController.swift
+++ FoundationsApp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet weak var storyTableView: UITableView!
     
-    let story1 = Story(imageReceived: #imageLiteral(resourceName: "tullawine"), headlineReceived: "Lorem Ipsum", briefReceived: "Lorem Ipsum", textReceived: "Lorem Ipsum")
+    let story1 = Story(imageReceived: #imageLiteral(resourceName: "tullawine"), headlineReceived: "Lorem Ipsum", briefReceived: "Lorem Ipsum", textReceived: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? \n\nSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?")
     let story2 = Story(imageReceived: #imageLiteral(resourceName: "example"), headlineReceived: "Lorem Ipsum", briefReceived: "Lorem Ipsum", textReceived: "Lorem Ipsum")
     let story3 = Story(imageReceived: #imageLiteral(resourceName: "tullawine"), headlineReceived: "Lorem Ipsum", briefReceived: "Lorem Ipsum", textReceived: "Lorem Ipsum")
     
@@ -28,10 +28,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         storyTableView.delegate = self
         storyTableView.dataSource = self
     }
+    
+    // Quantidade de cells
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return stories.count
     }
+    
+    // Populando a UITableView
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = storyTableView.dequeueReusableCell(withIdentifier: "storyCell", for: indexPath) as! StoryTableViewCell
@@ -45,17 +49,22 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
+    // Tamanho da TableViewCell
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 350
     }
     
+    // Controle da Segue showDetailsSegue
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "" {
+        if segue.identifier == "showDetailsSegue" {
             let index = storyTableView.indexPathForSelectedRow!
             let storySelected = stories[index.row]
-            let nextDetailStoryViewController = segue.destination as! DetailStoryViewController
-            nextDetailStoryViewController.detailStory = storySelected
+            let showDetailStoryViewController = segue.destination as! DetailStoryViewController
             
+            showDetailStoryViewController.detailStory = storySelected
+        }
             
     }
 
