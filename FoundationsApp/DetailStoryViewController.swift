f34035a4eab2169c36846b68e7fdf973e4f3eb13
Manu <emanuela-cordeiro@hotmail.com>
2018-10-08 15:12:36 -0300

    Atualizaçao detailstory

---
+++ FoundationsApp/DetailStoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailStoryViewController.swift
+//  FoundationsApp
+//
+//  Created by EMANUELA CORDEIRO DE SOUSA on 05/10/2018.
+//  Copyright © 2018 Elza. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailStoryViewController: UIViewController {
+    
+    var detailStory: Story!
+    
+    
+    @IBOutlet weak var detailImage: UIImageView!
+    @IBOutlet weak var detailHeadline: UILabel!
+    @IBOutlet weak var detailDescription: UILabel!
+    @IBOutlet weak var detailText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+}
+
