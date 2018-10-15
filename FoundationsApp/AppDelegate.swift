e3a046e212c1fde78578faed914c53e0b020c252
Manu <emanuela-cordeiro@hotmail.com>
2018-10-15 17:12:05 -0300

    Cor contato

--- FoundationsApp/AppDelegate.swift
+++ FoundationsApp/AppDelegate.swift
@@ -12,10 +12,16 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var navigationBarAppearance = UINavigationBar.appearance()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        navigationBarAppearance.tintColor = UIColorFromHex("cb4f8c")
+        
+        // change navigation item title color
+        navigationBarAppearance.titleTextAttributes = [NSForegroundColorAttributeName:UIColorFromHex("cb4f8c")]
+        
         return true
     }
 
@@ -40,6 +46,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    
 
 
 }
